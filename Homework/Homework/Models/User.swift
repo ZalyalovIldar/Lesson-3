3a166ef220a12581b344845bed443ea28ebd15d8
Roman Shurkin <roma.shurkin@mail.ru>
2019-10-07 22:25:26 +0300

    add homework

---
+++ Homework/Homework/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Homework
+//
+//  Created by Роман Шуркин on 04/10/2019.
+//  Copyright © 2019 Роман Шуркин. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class User {
+    
+    var name: String
+    var avatar: UIImage
+    
+    init(name: String, avatar: UIImage) {
+    
+        self.name = name
+        self.avatar = avatar
+    }
+}
